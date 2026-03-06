19877fe627f4f9b5674d48de1c203e448acb5e03
Diogo Martuchele <diogomartuchele@gmail.com>
2026-03-06 15:22:07 +0000

    File changes

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { 
   Sparkles,
